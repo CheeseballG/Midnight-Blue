04280e2962c7b2bd0cf9b04325549391870e48c6
Gamer-Gaming-Gamer <coopthecrafter@icloud.com>
2021-06-29 14:35:47 -0500

    Added placeholder code for links to fights

--- JSClicker.js
+++ JSClicker.js
@@ -1,15 +1,14 @@
 //Javascript
 var clicked = false;
-var scrap = 0;
+var scrappp = 0;
 var scrapbutton = document.getElementById("cookie");
 scrapbutton.addEventListener("click", function clicker() {
-scrap++
+scrappp++
 });
 
-    if (scrap = 50) {window.location.href = "fileforfight"}
+    if (scrappp = 50) {window.location.href = "fileforfight"}
 
 //log when user clicks on png, then award them with scrap
 
 // once goal is reached, send user to boss with stats
 
-//log when users click on an item in the shop, and make it so they have to pay scrap for their item

c31ca390102e0be58f710a5d9cbe49df5c7b5bef
Gamer-Gaming-Gamer <coopthecrafter@icloud.com>
2021-06-29 15:52:41 -0500

    Fixed a bug

--- JSClicker.js
+++ JSClicker.js
@@ -4,40 +4,41 @@ var cookiecount = 0;
             function Click() {
                cookiecount = cookiecount + 1
                document.getElementById('text').value = cookiecount; 
+               Check();
             }
             function Check() {
-                if (cookiecount = 50) {
-                    if (fights = 0) {
+                if (cookiecount == 50) {
+                    if (fights == 0) {
                         alert("The boss is here! Go to the other window to slay it!")
                         window.open = "robofight.html"
                         fights++
                     }
                 }
-                if (cookiecount = 250) {
-                if (fights = 1) {
+                if (cookiecount == 250) {
+                if (fights == 1) {
                     alert("The boss is here! Go to the other window to slay it!")
                     window.open = "robofight.html"
                     fights++
 
+                    }
                 }
-                }
-                if (cookiecount = 500) {
-                    if (fights = 2) {
+                if (cookiecount == 500) {
+                    if (fights == 2) {
                         alert("The boss is here! Go to the other window to slay it!")
                         window.open = "robofight.html"
                         fights++
                         cookiecount = 0
                     }
                 }
-                if (cookiecount = 1000) {
-                    if (fights = 3) {
+                if (cookiecount == 1000) {
+                    if (fights == 3) {
                         alert("The boss is here! Go to the other window to slay it!")
                         window.open = "robofight.html"
                         fights++
                         cookiecount = 0
                     }
                 }
-                if (fights = 4) {
+                if (fights == 4) {
                     alert("You did it! You win!")
                 }
             }

c4f5359383e3309473c4e7fa8ccfcd014429d76d
Gamer-Gaming-Gamer <coopthecrafter@icloud.com>
2021-06-29 13:58:50 -0500

    first JS file for clikers

---
+++ JSClicker.js
@@ -0,0 +1,15 @@
+//Javascript
+var clicked = false;
+var scrap = 0;
+var scrapbutton = document.getElementById("cookie");
+scrapbutton.addEventListener("click", function clicker() {
+scrap++
+});
+
+    if (scrap = 50) {window.location.href = "fileforfight"}
+
+//log when user clicks on png, then award them with scrap
+
+// once goal is reached, send user to boss with stats
+
+//log when users click on an item in the shop, and make it so they have to pay scrap for their item
